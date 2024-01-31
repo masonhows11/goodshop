8458c3f3475eafe9aa7c558d8272bebdadc1a1d2
mason.hows11 <mason.hows11@gmail.com>
2024-01-31 06:33:15 +0330

    change livewire name in front end layout

--- app/Http/Livewire/Front/Layout/FrontCategorySlider.php
+++ app/Http/Livewire/Front/Layout/FrontCategorySlider.php
@@ -2,45 +2,15 @@
 
 namespace App\Http\Livewire\Front\Layout;
 
-
 use App\Models\Category;
-use App\Models\ProductByCategory;
 use Livewire\Component;
-
 
 class FrontCategorySlider extends Component
 {
-    public $category;
-    public $products;
-    public $category_name;
-
-    public function mount()
-    {
-        $this->category = ProductByCategory::select('category_id', 'category_name')->first();
-
-        $this->category_name = $this->category ? $this->category->category_name : null;
-        $this->products = $this->category ? Category::find($this->category->category_id)
-            ->products()->take(6)
-            ->get() : null;
-        $this->products = collect($this->products)->where('status', 1)->all();
-
-
-    }
-
-    //    public function addToFave()
-    //    {
-    //        if (!Auth::check()) {
-    //            return redirect()->route('auth.login.form');
-    //        } else {
-    //
-    //        }
-    //    }
-
     public function render()
     {
         return view('livewire.front.layout.front-category-slider')
-            ->with(['products' => $this->products, 'category_name' => $this->category_name]);
-
-
+            ->with(['categories' => Category::select(['image_path','title_persian'])
+            ->where(['parent_id' => null,'status'=>1])->get()]);
     }
 }
