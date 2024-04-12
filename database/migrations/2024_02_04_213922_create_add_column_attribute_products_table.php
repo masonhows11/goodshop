4366732e36fbd6a6c3580f035deef91a8ad100dc
mason.hows11 <mason.hows11@gmail.com>
2024-04-12 10:52:38 +0330

    updated

---
+++ database/migrations/2024_02_04_213922_create_add_column_attribute_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attribute_products', function (Blueprint $table) {
+             $table->id();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedBigInteger('attribute_id')->nullable();
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
+
+            $table->string('type')->nullable();
+            $table->text('values')->nullable();
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attribute_products', function (Blueprint $table) {
+            //
+        });
+    }
+};
