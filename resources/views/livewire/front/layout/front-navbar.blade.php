8bb7f07cb821e6b6ae0099447c7e68c135e566d7
mason.hows11 <mason.hows11@gmail.com>
2024-06-17 16:56:32 +0330

    front navbar style updated

--- resources/views/livewire/front/layout/front-navbar.blade.php
+++ resources/views/livewire/front/layout/front-navbar.blade.php
@@ -2,8 +2,8 @@
 
     <header class="d-lg-none bg-white w-100">
         <div class="container">
-            <div class="row py-2">
-                <div class="col-7 d-flex flex-wrap">
+            <div class="row d-flex py-2">
+                <div class="col-6 d-flex flex-wrap">
                     <a href="#mobile-menu" data-bs-toggle="offcanvas">
                         <i class="fa fa-bars mobile-menu-icon mt-2"></i>
                     </a>
@@ -59,11 +59,21 @@
                 </div>
 
                 @guest
-                    <div class="col-4 d-flex align-items-center justify-content-end ">
+
+                    <div class="col-4 d-flex align-items-center justify-content-end">
                         <a class="text-dark" href="{{ route('auth.login.form') }}">ورود / ثبت نام</a>
                     </div>
+
+                    <div class="col-2 d-flex align-items-center justify-content-end">
+                        <a href="{{ route('auth.login.form') }}" class="position-relative">
+                            <img src="front/images/cart.png">
+                            <div class="count">0</div>
+                        </a>
+                    </div>
+
                 @endguest
                 @auth
+
                     <div class="col-4 d-flex align-items-center justify-content-end">
                         <div class="dropdown">
                             <a href="#" data-bs-toggle="dropdown"><i class="fa fa-user-lock signup-login-icon"></i></a>
@@ -94,6 +104,87 @@
                                             class="fas fa-sign-out-alt text-muted font-14 me-1"></i>خروج از حساب کاربری</a>
                                 </li>
                             </ul>
+                        </div>
+                    </div>
+
+
+                    <div class="col-1 d-flex align-items-center justify-content-end">
+                        <a href="#shopping-cart-responsive" class="position-relative" data-bs-toggle="offcanvas">
+                            <img src="front/images/cart.png">
+                            <div class="count">2</div>
+                        </a>
+                        <div class="offcanvas offcanvas-end" tabindex="-1" data-bs-scroll="true" id="shopping-cart-responsive">
+
+                            <div class="offcanvas-header">
+                                <p class="offcanvas-title font-12">سبد خرید (4 کالا)</p>
+                                <button type="button" class="text-reset btn-close" data-bs-dismiss="offcanvas"></button>
+                            </div>
+
+                            <div class="offcanvas-body">
+
+                                <div class="row">
+                                    <div class="col-4"><img src="front/images/mobile1.jpg" class="img-fluid img-thumbnail"></div>
+                                    <div class="col-8 d-flex align-items-center">
+                                        <a href="product.html" class="cart-product-title">گوشی موبایل سامسونگ مدل Galaxy A21S SM-A217F/DS</a>
+                                    </div>
+                                </div>
+                                <div class="row my-3 border-bottom">
+                                    <div class="col-6 d-flex">
+                                        <span class="number">1 عدد</span>
+                                        <span class="color" style="background-color:#d4d4d4;"></span>
+                                        <i class="fa fa-trash cart-delete-btn"></i>
+                                    </div>
+                                    <div class="col-6 d-flex justify-content-end">
+                                        <p class="cart-product-price">4,169,000 تومان</p>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-4"><img src="front/images/mobile2.jpg" class="img-fluid img-thumbnail"></div>
+                                    <div class="col-8 d-flex align-items-center">
+                                        <a href="product.html" class="cart-product-title">گوشی موبایل سامسونگ مدل Galaxy A21S SM-A217F/DS</a>
+                                    </div>
+                                </div>
+                                <div class="row my-3 border-bottom">
+                                    <div class="col-6 d-flex">
+                                        <span class="number">1 عدد</span>
+                                        <span class="color" style="background-color:#d4d4d4;"></span>
+                                        <i class="fa fa-trash cart-delete-btn"></i>
+                                    </div>
+                                    <div class="col-6 d-flex justify-content-end">
+                                        <p class="cart-product-price">4,169,000 تومان</p>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-4"><img src="front/images/mobile3.jpg" class="img-fluid img-thumbnail"></div>
+                                    <div class="col-8 d-flex align-items-center">
+                                        <a href="product.html" class="cart-product-title">گوشی موبایل سامسونگ مدل Galaxy A21S SM-A217F/DS</a>
+                                    </div>
+                                </div>
+                                <div class="row my-3 border-bottom">
+                                    <div class="col-6 d-flex">
+                                        <span class="number">1 عدد</span>
+                                        <span class="color" style="background-color:#d4d4d4;"></span>
+                                        <i class="fa fa-trash cart-delete-btn"></i>
+                                    </div>
+                                    <div class="col-6 d-flex justify-content-end">
+                                        <p class="cart-product-price">4,169,000 تومان</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row cart-footer">
+                               <div class="col-5">
+                                    <p>مبلغ قابل پرداخت:</p>
+                                    <p>12,480,000 تومان</p>
+                               </div>
+                               <div class="col-7">
+                                <a href="login.html" class="btn btn-info font-13 btn-lg ms-4">ورود و ثبت سفارش</a>
+                               </div>
+                            </div>
+
                         </div>
                     </div>
                 @endauth
